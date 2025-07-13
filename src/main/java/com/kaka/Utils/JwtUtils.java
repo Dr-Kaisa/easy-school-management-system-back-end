4d2b7f8bec2943496dd9a140530f75952f927fda
Dr.J <1816087404@qq.com>
2025-07-13 21:25:02 +0800

    消除硬编码，现在可以在配置文件中配置jwt令牌密钥和保活时间了

--- src/main/java/com/kaka/Utils/JwtUtils.java
+++ src/main/java/com/kaka/Utils/JwtUtils.java
@@ -3,38 +3,49 @@ package com.kaka.Utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtUtils {
+    //先注入到临时变量
+    @Value("${jwt.key}")
+    private String configKey;
 
-    private static String signKey = "SVRIRUlNQQ==";
-    private static Long expire = 43200000L;
+    @Value("${jwt.expire}")
+    private Long configExpire;
+
+    private static String signKey;
+    private static Long expire;
+    //再赋值到静态变量中
+    @PostConstruct
+    public void init() {
+        JwtUtils.signKey = this.configKey;
+        JwtUtils.expire = this.configExpire;
+    }
 
     /**
      * 生成JWT令牌
-     * @return
      */
     public static String generateJwt(Map<String,Object> claims){
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .addClaims(claims)
                 .signWith(SignatureAlgorithm.HS256, signKey)
                 .setExpiration(new Date(System.currentTimeMillis() + expire))
                 .compact();
-        return jwt;
     }
 
     /**
      * 解析JWT令牌
-     * @param jwt JWT令牌
-     * @return JWT第二部分负载 payload 中存储的内容
      */
     public static Claims parseJWT(String jwt){
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .setSigningKey(signKey)
                 .parseClaimsJws(jwt)
                 .getBody();
-        return claims;
     }
 }
